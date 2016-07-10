8f7e0f6b2c544a394df617671fd8be9465706ffd
Kolokotronis Panagiotis <panos122008@gmail.com>
2016-07-10 12:20:56 +0300

    Delete sendUsers.sh

--- sendUsers.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-noUsers=$1
-currentDirectory=$(pwd)
-mosquitto_pub -h www.techministry.gr -u magicUsername -P uberPasswd --cafile $currentDirectory/MQTT-CA-TM.crt  -t "techministry/spacestatus/hackers" -m "$noUsers"
